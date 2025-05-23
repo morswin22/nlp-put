f9556c1fc6eb7a7eb2973c0390941ac670b18b6d
Patryk Janiak <xmorswinx@gmail.com>
2025-05-23 12:51:58 +0200

    use bert for sentiment analysis

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 import kagglehub
 import pandas as pd
 import os
+from transformers import pipeline
 
 path = kagglehub.dataset_download("lakshmi25npathi/imdb-dataset-of-50k-movie-reviews")
 
 data = pd.read_csv(os.path.join(path, "IMDB Dataset.csv"))
 
 print(data)
+
+sentiment_analyzer = pipeline("sentiment-analysis", model="distilbert-base-uncased-finetuned-sst-2-english")
+
+results = sentiment_analyzer(data['review'].iloc[:5].tolist())
+
+print(results)

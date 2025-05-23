282f6359411eaafbb11f428c7872bc6c690b077c
Patryk Janiak <xmorswinx@gmail.com>
2025-05-23 13:02:51 +0200

    fix flake8 with black

--- main.py
+++ main.py
@@ -3,14 +3,19 @@ import pandas as pd
 import os
 from transformers import pipeline
 
-path = kagglehub.dataset_download("lakshmi25npathi/imdb-dataset-of-50k-movie-reviews")
+path = kagglehub.dataset_download(
+    "lakshmi25npathi/imdb-dataset-of-50k-movie-reviews"
+)
 
 data = pd.read_csv(os.path.join(path, "IMDB Dataset.csv"))
 
 print(data)
 
-sentiment_analyzer = pipeline("sentiment-analysis", model="distilbert-base-uncased-finetuned-sst-2-english")
+sentiment_analyzer = pipeline(
+    "sentiment-analysis",
+    model="distilbert-base-uncased-finetuned-sst-2-english",
+)
 
-results = sentiment_analyzer(data['review'].iloc[:5].tolist())
+results = sentiment_analyzer(data["review"].iloc[:5].tolist())
 
 print(results)
